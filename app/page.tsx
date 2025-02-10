89798f8544febf0338645a958fe9f85b406fd13c
Isikawa <24aw0107@jec.ac.jp>
2025-02-10 14:15:00 +0900

    1/10

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import  styles  from '../app/app.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Top Twitch Streams</h1>
+      <p className={styles.preface}>現在Twitch配信中のトップストリーマーやクリップを見てみよう！
+      </p>
+      <div className={styles.explanationBox}>
+        <p className={styles.subtitle}>Twitchとは？</p>
+        <p className={styles.paragraph}>Twitchは、Amazonが運営するライブストリーミングプラットフォームです。</p>
+      </div>
+      <div className={styles.explanationBox}>
+        <p className={styles.subtitle}>トップストリーマーとは？</p>
+        <p className={styles.paragraph}>トップストリーマーは、Twitchで最も人気のある配信者のことです。</p>
+      </div>
+      <div className={styles.explanationBox}>
+        <p className={styles.subtitle}>クリップとは？</p>
+        <p className={styles.paragraph}>クリップは、Twitchの配信中に視聴者が面白い部分を切り取って保存したものです。</p>
+      </div>
     </div>
   );
 }
